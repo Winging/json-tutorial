d802817f836ec2ac9e55bed5f2b49dd0d92f01e1
amongv587 <hityouok@qq.com>
2016-10-08 22:58:03 +0800

    我的解决方案

--- tutorial02/leptjson.c
+++ tutorial02/leptjson.c
@@ -1,8 +1,11 @@
 #include "leptjson.h"
 #include <assert.h>  /* assert() */
 #include <stdlib.h>  /* NULL, strtod() */
+#include <string.h>
 
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
+#define ISDIGIT(ch)         ((ch) >= '0' && (ch) <= '9')
+#define ISDIGIT1TO9(ch)     ((ch) >= '1' && (ch) <= '9')
 
 typedef struct {
     const char* json;
@@ -41,10 +44,26 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
     v->type = LEPT_NULL;
     return LEPT_PARSE_OK;
 }
-
+static int lept_parse_literal(lept_context* c, lept_value* v, const char* literalV, lept_type vType){
+	if(strncmp(c->json, literalV, strlen(literalV) ) == 0){
+		v->type = vType;
+		c->json += strlen(literalV);
+		return LEPT_PARSE_OK;
+	}
+	v->type = LEPT_NULL;
+	return LEPT_PARSE_INVALID_VALUE;
+}
 static int lept_parse_number(lept_context* c, lept_value* v) {
     char* end;
     /* \TODO validate number */
+	//如果首位不是数字则返回非法值
+	if ((!ISDIGIT(*c->json)) && (*c->json !='-')){
+		return LEPT_PARSE_INVALID_VALUE;
+	}
+	//如果最后一位是标点符号，返回非法值
+	if ('.' == c->json[strlen(c->json) -1]){
+		return LEPT_PARSE_INVALID_VALUE;
+	}
     v->n = strtod(c->json, &end);
     if (c->json == end)
         return LEPT_PARSE_INVALID_VALUE;
@@ -55,9 +74,9 @@ static int lept_parse_number(lept_context* c, lept_value* v) {
 
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
-        case 't':  return lept_parse_true(c, v);
-        case 'f':  return lept_parse_false(c, v);
-        case 'n':  return lept_parse_null(c, v);
+        case 't':  return lept_parse_literal(c, v, "true", LEPT_TRUE);
+        case 'f':  return lept_parse_literal(c, v, "false", LEPT_FALSE);
+        case 'n':  return lept_parse_literal(c, v, "null", LEPT_NULL);
         default:   return lept_parse_number(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
     }
